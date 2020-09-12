2e245f670d2db27b79d0ebfa6df7419f28034bf6
Alexander <alexyammine94@gmail.com>
2020-09-12 13:21:08 -0400

    Removed -f flag, now cmd reads rootFolder from args

--- pkg/cmd/cli.go
+++ pkg/cmd/cli.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Alex99y/duplicate-files/pkg/utils"
+
 	"github.com/spf13/cobra"
 )
 
@@ -35,12 +37,20 @@ func (cmd *CobraInterface) setStart() {
 		Use:   "start",
 		Short: "Execute duplicate files searcher",
 		Long:  "Long description",
-		Run: func(c *cobra.Command, arg []string) {
-			cmd.RootFolder, _ = c.PersistentFlags().GetString("path")
+		Run: func(c *cobra.Command, args []string) {
+			cmd.RootFolder = args[0]
+		},
+		Args: func(c *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("No root folder provided")
+			}
+			isDir, err := utils.IsDirectory(args[0])
+			if err != nil || !isDir {
+				return fmt.Errorf("Invalid root folder provided")
+			}
+			return nil
 		},
 	}
-	start.PersistentFlags().StringP("path", "f", "", "--path /home")
-	start.MarkPersistentFlagRequired("path")
 
 	cmd.RootCmd.AddCommand(start)
 }
